6b6cb083f06ba175ef012d434eb2cf47776935d6
mtsao96 <mtsao@bayareametro.gov>
2025-02-25 15:57:39 -0800

    Added "FBP_MR_018_US101_BOS" to 2030 HWY

--- scripts/net_spec_blueprint.py
+++ scripts/net_spec_blueprint.py
@@ -375,7 +375,7 @@ BLUEPRINT_PROJECTS = collections.OrderedDict([
                     #    'FBP_NP_040_VINE_Exp_Bus_Enhancements',
                     #    'FBP_AL_045_Oak_Ala_Access_Pr',
                     #    {'name':'FBP_MR_021_101_580_Direct_Connector',                                     'variants_exclude':['Alt1']},
-                    #    'FBP_MR_018_US101_BOS',
+                        'FBP_MR_018_US101_BOS',
                     #    'FBP_CC_036_I80_ExpBus_Impr',
                     #    {'name':'FBP_CC_040_041_042_I680_SR4_Int_Phases_1_2_4_5','kwargs':{'PHASE':"'4'"}, 'variants_exclude':['Alt1']},
                     #    'FBP_CC_021_Ant_Mart_Herc_Ferry',
